89f0018014f450b1a1c7e9b09d6d25ac9aa9015a
윤지영 <jyyun@idevel.kr>
2022-09-16 16:49:08 +0900

    detail/_id.vue head 추가

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -27,6 +27,16 @@ export default {
     const product = response.data;
     return { product }
   },
+   head: {
+    title: 'Shopping Item Detail',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: '이 상품은 ~~입니다.',
+      },
+    ]
+  },
   methods:{
     async addToCart(){
       const response = await createCartItem(this.product);
